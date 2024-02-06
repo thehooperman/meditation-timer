d2b198ec99a2ac42906269a801346112dec16351
craig <thehooperman@gmail.com>
2024-02-06 17:03:29 +0200

    update timer component
    add pause and stop buttons

--- components/timer/Timer.tsx
+++ components/timer/Timer.tsx
@@ -1,40 +1,65 @@
 import { useState, useEffect } from "react";
 import styles from "./Timer.module.scss";
 
-const Timer = ({ time1 = 1, time2 = 1, time3 = 1 }) => {
+const Timer = ({ time1 = 0.25, time2 = 0.25, time3 = 0.25 }) => {
   const [timer1, setTimer1] = useState(time1 * 60);
   const [timer2, setTimer2] = useState(time2 * 60);
   const [timer3, setTimer3] = useState(time3 * 60);
   const [finished, setFinished] = useState(false);
+  const [currentTimer, setCurrentTimer] = useState(1);
+  const [isPaused, setIsPaused] = useState(false);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (timer1 > 0) {
-        setTimer1((prevTime) => prevTime - 1);
-      }
-      if (timer2 > 0 && timer1 === 0) {
-        setTimer2((prevTime) => prevTime - 1);
-      }
-      if (timer3 > 0 && timer1 === 0 && timer2 === 0) {
-        setTimer3((prevTime) => prevTime - 1);
-      }
-    }, 1000);
+    let interval: NodeJS.Timeout;
+
+    if (!isPaused) {
+      interval = setInterval(() => {
+        if (currentTimer === 1) {
+          if (timer1 > 0) {
+            setTimer1((prevTime) => prevTime - 1);
+          } else {
+            setCurrentTimer(2);
+          }
+        } else if (currentTimer === 2) {
+          if (timer2 > 0) {
+            setTimer2((prevTime) => prevTime - 1);
+          } else {
+            setCurrentTimer(3);
+          }
+        } else if (currentTimer === 3) {
+          if (timer3 > 0) {
+            setTimer3((prevTime) => prevTime - 1);
+          } else {
+            setCurrentTimer(0);
+            setFinished(true);
+          }
+        }
+      }, 1000);
+    }
 
     return () => clearInterval(interval);
-  }, [timer1, timer2, timer3]);
+  }, [currentTimer, timer1, timer2, timer3, isPaused]);
 
-  useEffect(() => {
-    if (timer1 === 0 && timer2 === 0 && timer3 === 0) {
-      setFinished(true);
-    }
-  }, [timer1, timer2, timer3]);
+  const handlePause = () => {
+    setIsPaused((prevState) => !prevState);
+  };
+
+  const handleStop = () => {
+    setCurrentTimer(0);
+    setFinished(true);
+    setTimer1(0);
+    setTimer2(0);
+    setTimer3(0);
+  };
 
   return (
     <div>
-      <div>Timer 1: {timer1}</div>
-      <div>Timer 2: {timer2}</div>
-      <div>Timer 3: {timer3}</div>
-      {finished && <div>Finished</div>}
+      {currentTimer === 1 && <div>Timer 1: {timer1}</div>}
+      {currentTimer === 2 && <div>Timer 2: {timer2}</div>}
+      {currentTimer === 3 && <div>Timer 3: {timer3}</div>}
+      {currentTimer === 0 && finished && <div>Finished</div>}
+      <button onClick={handlePause}>{isPaused ? "Resume" : "Pause"}</button>
+      <button onClick={handleStop}>Stop</button>
     </div>
   );
 };
